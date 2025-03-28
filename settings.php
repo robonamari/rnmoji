358d25723302eab765ce17ecd629f30520e88246
robonamari <ROBONAMARI@GMAIL.COM>
2025-03-29 01:50:48 +0330

    Refactor plugin action links function for improved readability and consistency

--- settings.php
+++ settings.php
@@ -1,9 +1,13 @@
 <?php
-function rnmoji_plugin_action_links(array $links, string $file): array {
+function rnmoji_plugin_action_links(array $links, string $file): array
+{
     if ($file === plugin_basename(__FILE__)) {
-        $settings_link = '<a href="' . admin_url('plugins.php?page=rnmoji-settings') . '">تنظیمات</a>';
+        $settings_link =
+            '<a href="' .
+            admin_url("plugins.php?page=rnmoji-settings") .
+            '">تنظیمات</a>';
         array_unshift($links, $settings_link);
     }
     return $links;
 }
-add_filter('plugin_action_links', 'rnmoji_plugin_action_links', 10, 2);
+add_filter("plugin_action_links", "rnmoji_plugin_action_links", 10, 2);
